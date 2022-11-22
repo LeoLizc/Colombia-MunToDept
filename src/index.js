b6cbe2c82bfb53ff594875ed1f10813cf87db5c7
LeoLizc <ldlizcano@uninorte.edu.co>
2022-11-22 12:48:26 -0500

    no sé5

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
 app.set('port', process.env.PORT || 5000);
 
 // uses
-app.use(express.json())
+// app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(morgan('dev'))
