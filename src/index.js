c24422ec7c28649d82064ce76ba2f58ad99c8567
LeoLizc <ldlizcano@uninorte.edu.co>
2022-11-22 11:54:29 -0500

    no sé

--- src/index.js
+++ src/index.js
@@ -19,23 +19,28 @@ const map = new Map();
 // library to concatenate paths
 const path = require('path');
 
+// log all the files in the directory
+fs.readdirSync(__dirname).forEach(file => {
+    console.log(file);
+});
+
 // concatenating the path
-fs.createReadStream(path.join(__dirname, 'Departamentos_y_Municipios_de_Colombia.csv'))
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        // console.log(results);
-        // convert the csv file to map with the format {municipio: departamento}
-        results.forEach((item) => {
-            // make sure the municipio is lower case and without accents
-            // and department is also without accents
-            const municipio = item.MUNICIPIO.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-            const departamento = item.DEPARTAMENTO.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-            map.set(municipio, departamento);
-            // console.log(results);
-            // map.set(item.Municipio, item.Departamento);
-        });
-    });
+// fs.createReadStream('Departamentos_y_Municipios_de_Colombia.csv')
+//     .pipe(csv())
+//     .on('data', (data) => results.push(data))
+//     .on('end', () => {
+//         // console.log(results);
+//         // convert the csv file to map with the format {municipio: departamento}
+//         results.forEach((item) => {
+//             // make sure the municipio is lower case and without accents
+//             // and department is also without accents
+//             const municipio = item.MUNICIPIO.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+//             const departamento = item.DEPARTAMENTO.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+//             map.set(municipio, departamento);
+//             // console.log(results);
+//             // map.set(item.Municipio, item.Departamento);
+//         });
+//     });
     
 console.log(map);
 
